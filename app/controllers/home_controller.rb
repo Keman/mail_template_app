2fbd61535c1d838103a7cc9c1812fe8dd01c4875
Keman <kemanorel23@gmail.com>
2017-09-05 23:54:30 +0500

    fix: remove forgotten comment

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
     account = Account.find(params[:account])
     message = Message.find(params[:message])
 
-    # AccountMailer.notify(message, account).deliver_now
+    AccountMailer.notify(message, account).deliver_now
 
     flash[:notice] = "Message with title #{message.title} was successfully sended to #{account.email}"
     redirect_to root_path
